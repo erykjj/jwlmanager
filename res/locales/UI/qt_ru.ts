a47866bfe04a13eb1cc91ad53bbe2979f63acfb2
Eryk J <infiniti@inventati.org>
2023-01-03 08:49:33 -0500

    Restructure Qt translation files

---
+++ res/locales/UI/qt_ru.ts
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru">
+<dependencies>
+<dependency catalog="qtbase_ru"/>
+<dependency catalog="ui_ru"/>
+</dependencies>
+</TS>
