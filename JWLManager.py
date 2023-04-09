edd8fa8bba22096af29af07c9778e0d85ef9f506
Eryk J <infiniti@inventati.org>
2023-04-09 09:03:38 -0400

    Enable Portuguese

--- JWLManager.py
+++ JWLManager.py
@@ -27,7 +27,7 @@
 """
 
 APP = 'JWLManager'
-VERSION = 'v2.2.2'
+VERSION = 'v2.2.3'
 
 import argparse, gettext, json, os, random, regex, shutil, sqlite3, sys, tempfile, traceback, uuid
 import pandas as pd
@@ -56,7 +56,7 @@ def get_language():
         'fr': 'French (français)',
         'it': 'Italian (italiano)',
         # 'pl': 'Polish (Polski)',
-        # 'pt': 'Portuguese (português)',
+        'pt': 'Portuguese (português)',
         # 'ru': 'Russian (pусский)',
         # 'zh': 'Chinese (中文)',
         }
