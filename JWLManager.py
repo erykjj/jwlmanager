eb4d173b6bdcbcd247b58dd321d2b07bc1166714
Eryk J <107011362+erykjj@users.noreply.github.com>
2023-08-25 15:19:57 -0400

    Fix for empty fields in Bible header note export

--- JWLManager.py
+++ JWLManager.py
@@ -27,7 +27,7 @@
 """
 
 APP = 'JWLManager'
-VERSION = 'v2.4.1'
+VERSION = 'v2.4.2'
 
 import argparse, gettext, json, os, regex, shutil, sqlite3, sys, uuid
 import pandas as pd
@@ -1267,6 +1267,8 @@ class ExportItems():
         # note in book header - similar to a publication
         for row in self.cur.execute(f"SELECT l.MepsLanguage, l.KeySymbol, l.BookNumber, l.ChapterNumber, n.BlockIdentifier, u.ColorIndex, n.Title, n.Content, GROUP_CONCAT(t.Name), n.LastModified, b.StartToken, b.EndToken {columns} FROM Note n JOIN Location l USING (LocationId) LEFT JOIN TagMap tm USING (NoteId) LEFT JOIN Tag t USING (TagId) LEFT JOIN UserMark u USING (UserMarkId) LEFT JOIN BlockRange b USING ( UserMarkId ) WHERE n.BlockType =1 AND l.BookNumber IS NOT NULL AND NoteId IN {self.items} GROUP BY n.NoteId;"):
             color = str(row[5] or 0)
+            title = row[6] or ''
+            content = row[7] or ''
             tags = row[8] or ''
             if row[10] != None:
                 rng = '{RANGE='+f'{row[10]}-{row[11]}'+'}'
@@ -1276,7 +1278,7 @@ class ExportItems():
                 created = '{CREATED='+row[12][:10]+'}'
             else:
                 created = ''
-            txt = '\n==={CAT=BIBLE}{LANG='+str(row[0])+'}{ED='+str(row[1])+'}{BK='+str(row[2])+'}{CH='+str(row[3])+'}{VER='+str(row[4])+'}{DOC=0}{COLOR='+color+'}'+rng+'{TAGS='+tags+'}'+created+'{MODIFIED='+row[9][:10]+'}===\n'+row[6]+'\n'+row[7].rstrip()
+            txt = '\n==={CAT=BIBLE}{LANG='+str(row[0])+'}{ED='+str(row[1])+'}{BK='+str(row[2])+'}{CH='+str(row[3])+'}{VER='+str(row[4])+'}{DOC=0}{COLOR='+color+'}'+rng+'{TAGS='+tags+'}'+created+'{MODIFIED='+row[9][:10]+'}===\n'+title+'\n'+content.rstrip()
             self.export_file.write(txt)
 
     def export_publications(self, columns):
