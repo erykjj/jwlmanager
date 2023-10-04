7b28a2905f4c66e468f0d1890347d57ce337add0
Eryk J. <107011362+erykjj@users.noreply.github.com>
2023-10-04 10:45:57 -0400

    Restore NO TITLE marking in Data Viewer

--- JWLManager.py
+++ JWLManager.py
@@ -1877,7 +1877,7 @@ class PreviewItems():
         for row in self.cur.execute(sql):
             item = {
                 'TYPE': row[0],
-                'TITLE': row[1],
+                'TITLE': row[1] or '* '+_('NO TITLE')+' *',
                 'NOTE': row[2].rstrip() or '',
                 'TAGS': row[3],
                 'BK': row[5],
