e32e104a0f57e10283c28228c629c7af0946c54d
Eryk J. <107011362+erykjj@users.noreply.github.com>
2024-01-13 11:02:44 -0500

    Update translations

---
+++ res/locales/UI/qt_pt.ts
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt">
+    <dependencies>
+        <dependency catalog="qtbase_pt"/>
+        <dependency catalog="ui_pt"/>
+    </dependencies>
+</TS>
---
+++ res/locales/UI/qt_uk.ts
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="uk">
+    <dependencies>
+        <dependency catalog="qtbase_uk"/>
+        <dependency catalog="ui_uk"/>
+    </dependencies>
+</TS>
