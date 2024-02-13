0d616112de316b67b6e9db8ba00113717d82b346
Eryk J. <107011362+erykjj@users.noreply.github.com>
2024-02-13 08:09:18 -0500

    Add Hungarian Qt translations

---
+++ res/locales/UI/qt_hu.ts
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hu">
+    <dependencies>
+        <dependency catalog="qtbase_hu"/>
+        <dependency catalog="ui_hu"/>
+    </dependencies>
+</TS>
