08435409e3412b3700528ede33b4b7c58c9bca23
Eryk J. <107011362+erykjj@users.noreply.github.com>
2024-05-01 11:24:51 -0400

    Add DropList for playlist images drag-and-drop

--- res/ui_extras.py
+++ res/ui_extras.py
@@ -29,7 +29,7 @@ from datetime import datetime
 
 from PySide6.QtCore import QSize, Qt, Signal
 from PySide6.QtGui import QAction, QIcon, QPixmap
-from PySide6.QtWidgets import QDialog, QDialogButtonBox, QFrame, QGridLayout, QHBoxLayout, QLabel, QLineEdit, QPlainTextEdit, QPushButton, QScrollArea, QSizePolicy, QStackedLayout, QTextEdit, QToolBar, QToolButton, QVBoxLayout, QWidget
+from PySide6.QtWidgets import QDialog, QDialogButtonBox, QFrame, QGridLayout, QHBoxLayout, QLabel, QLineEdit, QListWidget, QPlainTextEdit, QPushButton, QScrollArea, QSizePolicy, QStackedLayout, QTextEdit, QToolBar, QToolButton, QVBoxLayout, QWidget
 
 
 _base_path = path.dirname(__file__)
@@ -99,6 +99,41 @@ class HelpBox(QDialog):
         self.setWindowState((self.windowState() & ~Qt.WindowMinimized) | Qt.WindowActive)
         self.finished.connect(self.hide())
 
+
+class DropList(QListWidget):
+    def __init__(self):
+        super(DropList, self).__init__()
+        self.setAcceptDrops(True)
+        self.files = []
+
+    def add_file(self, f):
+        self.addItem(f)
+        self.files.append(f)
+
+    def clear_files(self):
+        self.files = []
+        self.clear()
+
+    def dragEnterEvent(self, event):
+        if event.mimeData().hasUrls():
+            event.acceptProposedAction()
+        else:
+            event.ignore()
+
+    def dragMoveEvent(self, event):
+        if event.mimeData().hasUrls():
+            event.acceptProposedAction()
+        else:
+            event.ignore()
+
+    def dropEvent(self, event):
+        md = event.mimeData()
+        if md.hasUrls():
+            for url in md.urls():
+                f = url.toLocalFile()
+                self.files.append(f)
+                self.addItem(f)
+            event.acceptProposedAction()
 
 class DataViewer(QDialog):
     escape_pressed = Signal()
