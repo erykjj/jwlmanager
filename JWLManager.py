c9896fc45a5a9068e2cc6816627f3144a69de3f2
Eryk J. <107011362+erykjj@users.noreply.github.com>
2024-10-05 06:24:57 -0400

    Rearrange statusbar message

--- JWLManager.py
+++ JWLManager.py
@@ -1740,7 +1740,7 @@ class Window(QMainWindow, Ui_MainWindow):
         if not count:
             self.statusBar.showMessage(' '+_('NOT imported!'), 3500)
             return
-        message = f' {count} {category} '+_('items imported/updated')
+        message = f' {category}: {count} '+_('items imported/updated')
         self.statusBar.showMessage(message, 3500)
         self.archive_modified()
         self.trim_db()
