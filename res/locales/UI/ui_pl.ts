fb3f119ef004594c62e0977fe9506886759e4d3c
Eryk J <infiniti@inventati.org>
2024-11-05 00:26:02 +0000

    Translated using Weblate (Polish)
    Currently translated at 100.0% (93 of 93 strings)
    
    Translation: JWLManager/Qt UI
    Translate-URL: https://hosted.weblate.org/projects/jwlmanager/qt-ui/pl/

--- res/locales/UI/ui_pl.ts
+++ res/locales/UI/ui_pl.ts
@@ -135,7 +135,7 @@
         <location filename="main_window.ui" line="531"/>
         <source>&amp;View</source>
         <oldsource>V&amp;iew</oldsource>
-        <translation type="unfinished">&amp;Widok</translation>
+        <translation>&amp;Widok</translation>
     </message>
     <message>
         <location filename="main_window.ui" line="541"/>
