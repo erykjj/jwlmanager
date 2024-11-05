00aae38992597f9ab90ce16694bf98cd1fa99c33
Eryk J <infiniti@inventati.org>
2024-11-05 00:26:35 +0000

    Translated using Weblate (Russian)
    Currently translated at 100.0% (93 of 93 strings)
    
    Translation: JWLManager/Qt UI
    Translate-URL: https://hosted.weblate.org/projects/jwlmanager/qt-ui/ru/

--- res/locales/UI/ui_ru.ts
+++ res/locales/UI/ui_ru.ts
@@ -125,7 +125,7 @@
         <location filename="main_window.ui" line="531"/>
         <source>&amp;View</source>
         <oldsource>V&amp;iew</oldsource>
-        <translation type="unfinished">&amp;Вид</translation>
+        <translation>&amp;Вид</translation>
     </message>
     <message>
         <location filename="main_window.ui" line="541"/>
