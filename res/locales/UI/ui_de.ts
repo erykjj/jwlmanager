43120fd05d04dd5aac8fb2578d673bd82c6efd57
Eryk J <infiniti@inventati.org>
2024-11-05 00:01:24 +0000

    Translated using Weblate (German)
    Currently translated at 84.9% (79 of 93 strings)
    
    Translation: JWLManager/Qt UI
    Translate-URL: https://hosted.weblate.org/projects/jwlmanager/qt-ui/de/

--- res/locales/UI/ui_de.ts
+++ res/locales/UI/ui_de.ts
@@ -323,17 +323,17 @@
         <location filename="main_window.ui" line="991"/>
         <source>&amp;Theme</source>
         <oldsource>T&amp;heme</oldsource>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Thema</translation>
     </message>
     <message>
         <location filename="main_window.ui" line="994"/>
         <source>Toggle theme</source>
-        <translation type="unfinished"></translation>
+        <translation>Thema umschalten</translation>
     </message>
     <message>
         <location filename="main_window.ui" line="997"/>
         <source>Ctrl+T</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+T</translation>
     </message>
     <message>
         <location filename="main_window.ui" line="798"/>
