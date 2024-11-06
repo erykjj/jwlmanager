931fea28b6ca6def71de3db62853226b048fa76b
Eryk J. <107011362+erykjj@users.noreply.github.com>
2024-11-06 13:45:03 -0500

    Fix typo

--- JWLManager.py
+++ JWLManager.py
@@ -2068,7 +2068,7 @@ class Window(QMainWindow, Ui_MainWindow):
             self.viewer_window.raise_()
 
         def clean_text(text):
-            return regex.sub(r'\p{Z}', ' ', text.trim())
+            return regex.sub(r'\p{Z}', ' ', text.strip())
 
         def process_issue(i):
             issue = str(i)
