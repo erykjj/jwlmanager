6a5888b82179078d0a504b106ca1529af9f19a50
Eryk J. <107011362+erykjj@users.noreply.github.com>
2025-01-24 17:32:10 -0500

    Fix current_archive bug

--- JWLManager.py
+++ JWLManager.py
@@ -1404,7 +1404,7 @@ class Window(QMainWindow, Ui_MainWindow):
             self.working_dir = Path(fname)
         else:
             self.working_dir = Path(fname).parent
-        current_archive = self.current_archive.name if self.current_archive else _('NEW ARCHIVE')
+        current_archive = self.current_archive if self.current_archive else _('NEW ARCHIVE')
         self.statusBar.showMessage(' '+_('Exporting. Please wait…'))
         app.processEvents()
         try:
