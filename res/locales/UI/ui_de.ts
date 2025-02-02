d717e86dbf6a57d0f047c1e27ab85e405aee7c83
Eryk J <infiniti@inventati.org>
2025-02-02 14:34:52 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (95 of 95 strings)
    
    Translation: JWLManager/Qt UI
    Translate-URL: https://hosted.weblate.org/projects/jwlmanager/qt-ui/de/

--- res/locales/UI/ui_de.ts
+++ res/locales/UI/ui_de.ts
@@ -303,21 +303,21 @@
         <location filename="main_window.ui" line="799"/>
         <source>Code</source>
         <oldsource>&amp;Code</oldsource>
-        <translation type="unfinished">Code</translation>
+        <translation>Codes</translation>
     </message>
     <message>
         <location filename="main_window.ui" line="813"/>
         <location filename="main_window.ui" line="816"/>
         <source>Short</source>
         <oldsource>&amp;Short</oldsource>
-        <translation type="unfinished">Kurz</translation>
+        <translation>Kurz</translation>
     </message>
     <message>
         <location filename="main_window.ui" line="827"/>
         <location filename="main_window.ui" line="830"/>
         <source>Full</source>
         <oldsource>&amp;Full</oldsource>
-        <translation type="unfinished">Volt</translation>
+        <translation>Volt</translation>
     </message>
     <message>
         <location filename="main_window.ui" line="1010"/>
