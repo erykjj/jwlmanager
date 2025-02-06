260e45510468fa8262454805311c89381a05c60d
Eryk J <infiniti@inventati.org>
2025-02-06 15:14:39 +0000

    Translated using Weblate (Japanese)
    Currently translated at 28.4% (27 of 95 strings)
    
    Translation: JWLManager/Qt UI
    Translate-URL: https://hosted.weblate.org/projects/jwlmanager/qt-ui/ja/

--- res/locales/UI/ui_ja.ts
+++ res/locales/UI/ui_ja.ts
@@ -331,43 +331,43 @@
         <location filename="main_window.ui" line="874"/>
         <source>English</source>
         <oldsource>&amp;English</oldsource>
-        <translation type="unfinished">English</translation>
+        <translation>English</translation>
     </message>
     <message>
         <location filename="main_window.ui" line="885"/>
         <source>Español</source>
         <oldsource>E&amp;spañol</oldsource>
-        <translation type="unfinished">Español</translation>
+        <translation>Español</translation>
     </message>
     <message>
         <location filename="main_window.ui" line="896"/>
         <source>Deutsch</source>
         <oldsource>&amp;Deutsch</oldsource>
-        <translation type="unfinished">Deutsch</translation>
+        <translation>Deutsch</translation>
     </message>
     <message>
         <location filename="main_window.ui" line="907"/>
         <source>Français</source>
         <oldsource>&amp;Français</oldsource>
-        <translation type="unfinished">Français</translation>
+        <translation>Français</translation>
     </message>
     <message>
         <location filename="main_window.ui" line="918"/>
         <source>Português</source>
         <oldsource>P&amp;ortuguês</oldsource>
-        <translation type="unfinished">Português</translation>
+        <translation>Português</translation>
     </message>
     <message>
         <location filename="main_window.ui" line="929"/>
         <source>Italiano</source>
         <oldsource>&amp;Italiano</oldsource>
-        <translation type="unfinished">Italiano</translation>
+        <translation>Italiano</translation>
     </message>
     <message>
         <location filename="main_window.ui" line="966"/>
         <source>Polski</source>
         <oldsource>&amp;Polski</oldsource>
-        <translation type="unfinished">Polski</translation>
+        <translation>Polski</translation>
     </message>
     <message>
         <location filename="main_window.ui" line="992"/>
