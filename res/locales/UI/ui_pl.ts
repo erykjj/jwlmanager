fb2437850d8f2cee827be45610ccd8460fbc4fc8
Eryk J <infiniti@inventati.org>
2025-02-06 22:45:05 +0000

    Translated using Weblate (Polish)
    Currently translated at 100.0% (97 of 97 strings)
    
    Translation: JWLManager/Qt UI
    Translate-URL: https://hosted.weblate.org/projects/jwlmanager/qt-ui/pl/

--- res/locales/UI/ui_pl.ts
+++ res/locales/UI/ui_pl.ts
@@ -330,12 +330,12 @@
     <message>
         <location filename="main_window.ui" line="1026"/>
         <source>&amp;Clean</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Czyścić</translation>
     </message>
     <message>
         <location filename="main_window.ui" line="1029"/>
         <source>Clean hidden characters</source>
-        <translation type="unfinished"></translation>
+        <translation>Usunąć ukryte znaki</translation>
     </message>
     <message>
         <location filename="main_window.ui" line="800"/>
