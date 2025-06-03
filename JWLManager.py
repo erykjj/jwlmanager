211023665094efd2d3bf2125cfb6b77c891ef543
Eryk J. <107011362+erykjj@users.noreply.github.com>
2025-06-02 21:16:03 -0400

    Fix setting background color in Note Editor

--- JWLManager.py
+++ JWLManager.py
@@ -2471,10 +2471,10 @@ class Window(QMainWindow, Ui_MainWindow):
             else:
                 self.viewer_window.meta.setHidden(True)
                 self.viewer_window.title.setReadOnly(True)
-            qss_class = self.note_item.text_box.property('background')
-            self.viewer_window.title.setProperty('background', qss_class)
+            qss_class = self.note_item.text_box.property('note')
+            self.viewer_window.title.setProperty('note', qss_class)
             self.viewer_window.title.setPlainText(self.note_item.title)
-            self.viewer_window.body.setProperty('background', qss_class)
+            self.viewer_window.body.setProperty('note', qss_class)
             self.viewer_window.body.setPlainText(self.note_item.body)
             app.processEvents()
             self.viewer_window.viewer_layout.setCurrentIndex(1)
