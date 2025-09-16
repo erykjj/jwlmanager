2e1cba2ee10fc5435f0048bdf9b636efc7396e50
Eryk J. <107011362+erykjj@users.noreply.github.com>
2025-09-16 18:39:14 -0400

    Remove unused languages

--- res/locales/UI/qt_ja.ts
+++
@@ -1,8 +0,0 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.1" language="ja">
-    <dependencies>
-        <dependency catalog="qtbase_ja"/>
-        <dependency catalog="ui_ja"/>
-    </dependencies>
-</TS>
